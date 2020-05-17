2bbaffb1e87cf1a9f48611f2ad229c93bb45da04
Alexandr <valverin95@gmail.com>
2020-05-17 21:54:21 +0300

    refactor: addTodo: create own hook for input

--- src/todo/AddTodo.js
+++ src/todo/AddTodo.js
@@ -1,21 +1,33 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 
+function useInput(defValue = '') {
+  const [value, setValue] = useState(defValue)
+
+  return {
+    bind: {
+      value,
+      onChange: ({target}) => setValue(target.value)
+    },
+    value: () => value,
+    reset: () => setValue('')
+  }
+}
+
 function AddTodo({ onCreate }) {
-  const [value, setValue] = useState('')
+  const input = useInput()
 
   function submitHandler(e) {
     e.preventDefault()
-    if (value.trim()) {
-      onCreate(value)
-      setValue('')
+    if (input.value().trim()) {
+      onCreate(input.value())
+      input.reset()
     }
-
   }
 
   return (
     <form onSubmit={submitHandler}>
-      <input type="text" value={value} onChange={({ target }) => setValue(target.value)} />
+      <input {...input.bind} />
       <button type="submit">Add Todo</button>
     </form>
   )
