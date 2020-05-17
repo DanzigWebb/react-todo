a412331e0db4e5d0c89447fb4c3afcae7293efd2
Alexandr <valverin95@gmail.com>
2020-05-18 00:38:45 +0300

    feat: filter: added resetBtn

--- src/todo/Filter/TodoFilter.js
+++ src/todo/Filter/TodoFilter.js
@@ -10,8 +10,15 @@ export default function TodoFilter() {
     Search$.next(value)
     setValue(value)
   }
+  function reset() {
+    Search$.next('')
+    setValue('')
+  }
 
   return (
-    <input className="filter__input" type="text" placeholder="search..." value={value} onChange={({target}) => onChange(target.value)}/>
+    <div className="filter">
+      <input className="filter__input" type="text" placeholder="search..." value={value} onChange={({target}) => onChange(target.value)}/>
+      <button className="filter__btn" onClick={() => reset()}>Reset</button>
+    </div>
   )
 }
